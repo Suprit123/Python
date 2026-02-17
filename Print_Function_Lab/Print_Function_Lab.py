ecf377b58f7f908b7a493d87924b81db70448049
Suprit123 <suprit.chinchodikar@gmail.com>
2026-02-17 11:22:50 +0530

    Modified

--- Print_Function_Lab/Print_Function_Lab.py
+++ Print_Function_Lab/Print_Function_Lab.py
@@ -1,5 +1,7 @@
-#In the below Print Function Lab, you will be using the print function to display various types of data.
-print("Hello, my name is Suprit")  # The content inside the parentheses is what will be printed to the console and its type is a string.
+# In the below Print Function Lab, you will be using the print function to display various types of data.
+print(
+    "Hello, my name is Suprit"
+)  # The content inside the parentheses is what will be printed to the console and its type is a string.
 
 # This line has been added to demonstrate the use of single quotes within a string.
 print("Welcome to the 'Python Print' Function Lab")
@@ -8,12 +10,14 @@ print("Welcome to the 'Python Print' Function Lab")
 print('Welcome to the "Python Print" Function Lab')
 
 # In below print function we will look into escape characters using backslash (\)
-print("Welcome to the \"CCNA\" Training")
+print('Welcome to the "CCNA" Training')
 
 # In below print function we will look into how to print multiple lines one after other by using backslash n (\n)
-print ("router ospf 10 \n router-id 1.1.1.1 \n network 192.168.1.0 0.0.0.255")
+print("router ospf 10 \n router-id 1.1.1.1 \n network 192.168.1.0 0.0.0.255")
 
-# The alternative for the above print function when we want to print multiple lines is use tripple quotes("""). We can use triple single quotes as well ('''). 
-print (""" router bgp 100 
+# The alternative for the above print function when we want to print multiple lines is use tripple quotes("""). We can use triple single quotes as well (''').
+print(
+    """ router bgp 100 
       neighbor 1.1.1.1 remote-as 100 
-      network 192.168.1.0 mask 255.255.255.0""")
+      network 192.168.1.0 mask 255.255.255.0 """
+)
