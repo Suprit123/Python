8bf7c2ff67152ad92ed2a918ced11399b01ae7db
Suprit123 <suprit.chinchodikar@gmail.com>
2026-02-21 10:54:10 +0530

    Multi Router Multi Command

--- Python-Prep/Lab-3_Multi_Routers_Multi_Commands/Lab3.Multi_Routers_Multi_Commands.py
+++ Python-Prep/Lab-3_Multi_Routers_Multi_Commands/Lab3.Multi_Routers_Multi_Commands.py
@@ -4,11 +4,12 @@ from netmiko import ConnectHandler
 # Import getpass library to securely get the password
 from getpass import getpass
 
-# Device hostname and ip address
-R1 = "172.16.166.129"
-R2 = "172.16.166.131"
+# Dictionary of the routers with their hostnames and ip addresses
 
-router_list = [R1, R2]
+router_dict = {
+    "R1": "172.16.166.129",
+    "R2": "172.16.166.131",
+}
 
 # List of commands to be sent to the routers
 commands = [
@@ -18,19 +19,21 @@ commands = [
 ]
 
 
-# Use for loop to connect to the routers as we have the list of routers in router_list
-for r in router_list:
+# Use a for loop with the items() method to iterate through the router_dict and get the hostname and ip add of each router
+for r_name, r_ip in router_dict.items():
+    print(f"=== Successfully connected to '{r_name}' === \n")
+
     # Get the username and password. Using the input() function to get the username from the user and getpass() function to get the password
     username = input("Enter your username: ")
     password = getpass("Enter your password: ")
+
     # Create ssh connection to the routers
     ssh = ConnectHandler(
         device_type="cisco_ios",
-        host=r,
+        host=r_ip,
         username=username,
         password=password,
     )
-    print(f"=== Successfully connected to '{r}' === \n")
 
     # Using the nested for loop to send the commands to the routers and print the output of the commands
     for c in commands:
