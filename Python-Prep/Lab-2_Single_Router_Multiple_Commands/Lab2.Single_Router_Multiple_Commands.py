d40a2b2caf50c5c2bc2243609a09b036d4b9d416
Suprit123 <suprit.chinchodikar@gmail.com>
2026-02-21 11:56:47 +0530

    Single Router Multi Command

--- Python-Prep/Lab-2_Single_Router_Multiple_Commands/Lab2.Single_Router_Multiple_Commands.py
+++ Python-Prep/Lab-2_Single_Router_Multiple_Commands/Lab2.Single_Router_Multiple_Commands.py
@@ -1,13 +1,8 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-# Device Paramaters
-R1 = {
-    "device_type": "cisco_ios",
-    "host": "172.16.166.129",
-    "username": "admin",
-    "password": getpass(),
-}
+# Using a dictionary to store the hostname and ip address of the device
+router_dict = {"R1": "172.16.166.131"}
 
 # Commands to be send to the device
 commands = [
@@ -16,16 +11,28 @@ commands = [
     "show version",
 ]
 
-# Connect to the device and send the commands
-ssh = ConnectHandler(**R1)
-print(f'=== Successfully connected to {R1["host"]} ===\n')
+# Use for loop with the items() method to iterate through the router_dict and get the hostname and ip add of each router
+for r_name, r_rip in router_dict.items():
+    print(f"=== Successfully connected to '{r_name}' === \n")
 
-# Use for loop as we have multiple commands to send and print the output
-for c in commands:
-    show_output = ssh.send_command(c)
-    # Print the output of the commands with the command name for better readability
-    print(f'=== Output of "{c}" ===\n{show_output}\n')
-    # Print a new line for better readability
-    print("\n")
-# Disconnect from the device
-ssh.disconnect()
+    # Get the username and password. Using the input() function to get the username from the user and getpass() function to get the password
+    username = input("Enter your username: ")
+    password = getpass("Enter your password: ")
+
+    # Connect to the device and send the commands
+    ssh = ConnectHandler(
+        device_type="cisco_ios",
+        host=r_rip,
+        username=username,
+        password=password,
+    )
+
+    # Use for loop as we have multiple commands to send and print the output
+    for c in commands:
+        show_output = ssh.send_command(c)
+        # Print the output of the commands with the command name for better readability
+        print(f'=== Output of "{c}" ===\n{show_output}\n')
+        # Print a new line for better readability
+        print("\n")
+    # Disconnect from the device
+    ssh.disconnect()
